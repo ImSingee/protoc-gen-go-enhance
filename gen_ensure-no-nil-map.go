0800a6c71eb54938e6eb3de3c8e5a5ecde182575
Singee <git@singee.me>
2021-12-16 13:49:06 +0800

    not generate EnsureNoNilMap for message which contains no map field

--- gen_ensure-no-nil-map.go
+++ gen_ensure-no-nil-map.go
@@ -3,6 +3,18 @@ package main
 import "google.golang.org/protobuf/compiler/protogen"
 
 func genForEnsureNoNilMap(gen *protogen.Plugin, g *protogen.GeneratedFile, f *fileInfo, m *messageInfo) {
+	containMap := false
+	for _, field := range m.Fields {
+		if field.Desc.IsMap() {
+			containMap = true
+			break
+		}
+	}
+
+	if !containMap {
+		return
+	}
+
 	g.P("func (x *", m.GoIdent, ") EnsureNoNilMap() {")
 	g.P("if x == nil { return }")
 
